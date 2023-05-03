c8504cfce274ad90179de7079b6d523ccc64d570
ZsakaiBalint <balintzsakai@gmail.com>
2023-05-03 15:55:19 +0200

    added tests

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Felhasználó/Desktop/MinesweeperSolver
+# Build directory: C:/Users/Felhasználó/Desktop/MinesweeperSolver/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
+subdirs("test")
+subdirs("src")
---
+++ test/windowTest.cpp
@@ -0,0 +1,11 @@
+#include <gtest/gtest.h>
+
+#include "../src/window.h"
+
+TEST(TestSuite, TestName) {
+    EXPECT_TRUE(true);
+}
+
+TEST(TestSuite, TestName2) {
+    EXPECT_EQ(1,1);
+}
