daf866ddd7028aee6fd3ed9d7b0b499c2d4819b7
ZsakaiBalint <balintzsakai@gmail.com>
2024-04-30 20:50:32 +0200

    fixed dependencies

--- src/main.cpp
+++ src/main.cpp
@@ -9,10 +9,12 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,PSTR lpCmdLine, 
     }
     catch (const std::exception& e) {
         std::cerr << "An exception occurred: " << e.what() << std::endl;
+        MessageBoxA(NULL, e.what(), "EXCEPTION", MB_OK);
         return 1; // Indicate failure
     }
     catch (...) {
         std::cerr << "An unknown exception occurred." << std::endl;
+        MessageBoxA(NULL, "UNKNOWN EXCEPTION", "UNKNOWN EXCEPTION", MB_OK);
         return 1; // Indicate failure
     }
 
